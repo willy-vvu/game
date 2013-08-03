afd213a6680a42af90a4809298609831929e662b
jtaylorchang <jtaylorchang@gmail.com>
2013-08-03 15:25:54 -0400

    Added fade to website text

--- website/main.js
+++ website/main.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
-	
+	$(".firstPhrase").animate({
+		opacity:"1"
+	}, 1500);
+	$(".firstPhrase2").delay(2100).animate({
+		opacity:"1"
+	}, 2000);
+	$(".firstPhrase3").delay(4200).animate({
+		opacity:"1"
+	}, 2000);
+	$(".firstPhrase4").delay(6300).animate({
+		opacity:"1"
+	}, 2000);
+	$(".firstPhrase5").delay(8400).animate({
+		opacity:"1"
+	}, 3000);
 });
